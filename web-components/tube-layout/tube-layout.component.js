c085fde3f62494dc0ba024e761aae14f4aab5f98
Diego Torres <diegotorre0303s@gmail.com>
2023-03-01 16:56:45 -0600

    BUMgit add -A

---
+++ web-components/tube-layout/tube-layout.component.js
@@ -0,0 +1,72 @@
+
+
+class TubeLayout extends HTMLElement {
+
+    constructor() {
+        super()
+    }
+
+    async _render() {
+        const inner = html`
+        
+        <link rel="stylesheet" href="style-tools.css">
+        <link rel="stylesheet" href="tube-layout/style.css">
+
+        <main class="super-container flex-row">
+        
+        <section class="main-section">
+
+            <video controls poster="https://external-preview.redd.it/STwA0sOSKsOWQkaJjqizA60k-CkBqa_8VnKuzK5BpPg.jpg?auto=webp&v=enabled&s=cca9d3ea128e72071cc46fc96a00c0ad98690260">
+                <source src="assets/patd-enc.mp4" type="video/mp4"/>
+            </video>
+            <div class="video-description flex-col jc-center ">
+                <h2>Emperor's New Clothes</h2>
+                <p>Panic! At The Disco</p>
+            </div>
+
+        </section>
+        <section class="lateral-section">
+            <button onclick="test" ></button>
+            <slot name="playlist"></slot>
+
+        </section>
+
+    </main>`
+        const shadow = this.attachShadow({mode: 'open'})
+        shadow.appendChild(inner)
+
+        const content = this
+        console.log(content)
+        // const inner = await html.import('test.component.html')
+
+        // replacing inline handler function with own component methods
+        mapComponentEvents(this, eventNames)
+
+        // get variable names
+        updateVars(this)
+
+    }
+
+    // test() {
+    //     alert('soy yo')
+    // }
+
+    connectedCallback() { this._render() }
+
+    disconnectedCallback() { }
+
+    attributeChangedCallback(name, oldValue, newValue) { }
+
+    adoptedCallback() { }
+
+}
+
+window.customElements.define('tube-layout', TubeLayout)
+
+
+
+
+
+
+
+
