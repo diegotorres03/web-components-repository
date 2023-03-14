ffe94dc8d1ef9df92fdb1e1ec6736a219d1d8a1f
codentide <marcodelboccio77@gmail.com>
2023-03-13 20:17:26 -0500

    fixing data-chart

--- web-components/cards/plain-card.component.js
+++ web-components/cards/plain-card.component.js
@@ -9,56 +9,18 @@ class PlainCard extends HTMLElement {
         const inner = html`
         <style>
 
-.cta-box {
-    width: 80%;
-    padding: 24px;
+.plain-card{
+    padding: 8px;
     border-radius: 4px;
 
     background-color: ghostwhite;
-    box-shadow: 0 4px 16px 4px #0000002d;
+    /* box-shadow: 0 4px 16px 4px #0000002d; */
 }
 
-.cta-box h1 {
-    font-size: 32px;
-    line-height: 32px;
-    /* margin-bottom: 12px; */
-}
-
-.cta-box p {
-    font-size: 16px;
-    margin: 12px 0;
-    line-height: 1.5;
-    color: var(--g2);
-}
-
-.cta-box .input-box input {
-    padding: 4px 12px;
-    border: 1px solid var(--b1);
-    background-color: transparent;
-    border-radius: 4px 0 0 4px;
-    width: 70%;
-}
-
-.cta-box .input-box input:focus {
-    outline: none;
-    background-color: var(--g3);
-}
-
-.cta-box .input-box button {
-    width: 30%;
-}
         </style>
-        <div class="cta-box">
+        <div class="plain-card">
+
             <slot></slot>
-<!--         
-            <h1>Subscribe with us now!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit orem ipsum dolor sit amet, consectetur
-                adipisicing elit.</p>
-        
-            <div class="input-box flex-row ">
-                <input type="text" class="cta-input" placeholder="example@email.com">
-                <button type="submit" class="btn cta-btn clickable">Get Started</button>
-            </div> -->
         
         </div>
         `
