9c43016b7439fad0f9290f903bfc1b58bce14b9a
codentide <marcodelboccio77@gmail.com>
2023-03-29 16:57:22 -0500

    Adding plain-card

---
+++ web-components/src/components/plain-card/src/plain-card.js
@@ -0,0 +1,35 @@
+import {
+  html,
+  mapComponentEvents,
+  updateVars,
+  registerTriggers,
+} from '../../../global/web-tools'
+
+import plainCardHtml from './plain-card.html'
+import plainCardStyle from './plain-card.css'
+
+export default class extends HTMLElement {
+  constructor() {
+    super()
+    const template = html` <style>
+        ${plainCardStyle}
+      </style>
+      ${plainCardHtml}`
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template)
+  }
+
+  connectedCallback() {
+    mapComponentEvents(this)
+    updateVars(this)
+    registerTriggers(this, (event) => console.log(event))
+  }
+
+  disconnectedCallback() {}
+
+  attributeChangedCallback(name, oldValue, newValue) {}
+
+  adoptedCallback() {}
+}
+
+window.customElements.define('')
