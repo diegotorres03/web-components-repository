d33ec76f7dadd134174837c3c85e5bc8700760a1
Diego Torres <diegotorre0303s@gmail.com>
2023-06-01 10:20:53 -0500

    asd

---
+++ web-components/src/layouts/landing/landing-layout.js
@@ -0,0 +1,37 @@
+import {
+  html,
+  registerTriggers,
+} from '../../global/web-tools'
+
+
+import componentHtml from './landing-layout.html'
+//import componentStyle from './flip-card.css'
+
+export default class LandingLayoutComponent extends HTMLElement {
+
+  constructor() {
+    super()
+    const template = html`
+      ${componentHtml}
+    `
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template)
+  }
+
+
+  connectedCallback() {
+    registerTriggers(this, (event) => console.log(event))
+  }
+
+  disconnectedCallback() { }
+
+  attributeChangedCallback(name, oldValue, newValue) { }
+
+  adoptedCallback() { }
+
+}
+
+
+
+
+window.customElements.define('landing-layout', LandingLayoutComponent)
---
+++ web-components/src/layouts/landing/notes.md
@@ -0,0 +1,14 @@
+# Header: 
+Debe llevar Logo, contacto, sección de información y el CTA
+
+# Hero: 
+Es una sección que tiene como utilidad el atraer la atención del usuario, debe ser llamativo, debe tener título, descripción, imagen o video y llamado a la acción CTA
+
+# Benefits: 
+Es una sección que muestra caracteristicas o beneficios sobre ventajas competitivas y cosas asi para convencer al usuario
+
+# Testimonials: 
+Carrusel de tarjetas de testimonios sobre el uso del producto
+
+# Cta: 
+Sección en donde se ingresa la información y se suscribe al servicio
