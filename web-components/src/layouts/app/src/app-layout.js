6f507cb17155a6404881677c11f07d39af4c595e
Marco Del Boccio <marcodelboccio@Diego-Torres.local>
2023-08-16 14:09:03 -0500

    Adding new feature on left-menu bar

--- web-components/src/layouts/app/src/app-layout.js
+++ web-components/src/layouts/app/src/app-layout.js
@@ -35,21 +35,84 @@ export default class AppLayoutComponent extends HTMLElement {
     `;
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template);
+    this.setupResize();
   }
 
-  showLeftContent() {
-    const leftContent = this.shadowRoot.querySelector(
-      '[data-left-content-hide]',
-    );
-    leftContent.style.display = 'flex';
-  }
+  // 15-08-23
+  // showLeftContent() {
+  //   const leftContent = this.shadowRoot.querySelector(
+  //     '[data-left-content-hide]',
+  //   );
+  //   leftContent.style.display = 'flex';
+  // }
 
-  hideLeftContent() {
-    const leftContent = this.shadowRoot.querySelector(
-      '[data-left-content-hide]',
-    );
-    leftContent.style.display = 'none';
-    console.log('leftContent', leftContent);
+  // hideLeftContent() {
+  //   const leftContent = this.shadowRoot.querySelector(
+  //     '[data-left-content-hide]',
+  //   );
+  //   leftContent.style.display = 'none';
+  //   console.log('leftContent', leftContent);
+  // }
+
+  setupResize() {
+    // this.querySelector || this.shadowRoot....
+    // Seleccionar los elementos del DOM
+    const resizerElement = this.shadowRoot.querySelector('.resizer');
+    const sideBarElement = this.shadowRoot.querySelector('.al-split__left');
+    const toggle = this.shadowRoot.querySelector('#hidder-btn');
+
+    // Función para inicializar el redimensionamiento
+    const initializeResizer = (resizerElement, sideBarElement) => {
+      let initialMouseX, initialWidth;
+      // Controlador de evento 'mousedown'
+      const handleMouseDown = (event) => {
+        // Almacenar la posición inicial del mouse en initialMouseX
+        initialMouseX = event.clientX;
+
+        // Obtener el ancho actual de la barra lateral
+        const sidebarWidth = window.getComputedStyle(sideBarElement).width;
+        initialWidth = parseInt(sidebarWidth, 10);
+
+        // Agregar listeners para los eventos 'mousemove' y 'mouseup'
+        this.shadowRoot.addEventListener('mousemove', handleMouseMove);
+        this.shadowRoot.addEventListener('mouseup', handleMouseUp);
+      };
+
+      // Controlador de evento 'mousemove'
+      const handleMouseMove = (event) => {
+        // Calcular el movimiento del mouse en el eje X
+        const mouseMovementX = event.clientX - initialMouseX;
+
+        // Calcular el nuevo ancho de la barra lateral
+        const newWidth = initialWidth + mouseMovementX;
+
+        // Limitar el ancho mínimo de la barra lateral a 700px
+        if (newWidth >= 50) {
+          sideBarElement.classList.remove('hidden');
+          sideBarElement.style.width = `${newWidth}px`;
+        } else {
+          sideBarElement.classList.add('hidden');
+        }
+      };
+
+      // Controlador de evento 'mouseup'
+      const handleMouseUp = () => {
+        // Eliminar los listeners de los eventos 'mousemove' y 'mouseup'
+        this.shadowRoot.removeEventListener('mousemove', handleMouseMove);
+        this.shadowRoot.removeEventListener('mouseup', handleMouseUp);
+      };
+
+      // Agregar el listener 'mousedown' al elemento resizer
+      resizerElement.addEventListener('mousedown', handleMouseDown);
+
+      toggle.addEventListener('click', function () {
+        sideBarElement.classList.toggle('hidden');
+        console.log('asda');
+      });
+    };
+
+    // Inicializar el redimensionamiento de la barra lateral
+    initializeResizer(resizerElement, sideBarElement);
   }
 
   connectedCallback() {
