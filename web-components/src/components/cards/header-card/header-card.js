b3cd0b5f2b81f4caf0ff683835a265bf0a0e2d2b
Marco Del Boccio <marcodelboccio77@gmail.com>
2023-11-09 18:12:08 -0500

    Adding accordion and modal rebuild

---
+++ web-components/src/components/cards/header-card/header-card.js
@@ -0,0 +1,35 @@
+import {
+  html,
+  mapComponentEvents,
+  updateVars,
+  registerTriggers,
+} from '../../../global/web-tools';
+
+import componentHtml from './header-card.html';
+import componentStyle from './header-card.css';
+
+export default class headerCard extends HTMLElement {
+  constructor() {
+    super();
+    const template = html`<style>
+        ${componentStyle}
+      </style>
+      ${componentHtml} `;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template);
+  }
+
+  connectedCallback() {
+    mapComponentEvents(this);
+    updateVars(this);
+    registerTriggers(this, (event) => console.log(event));
+  }
+
+  disconnectedCallback() {}
+
+  attributeChangedCallback(name, oldValue, newValue) {}
+
+  adoptedCallback() {}
+}
+
+window.customElements.define('header-card', headerCard);
