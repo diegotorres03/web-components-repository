d2bdf78a6aa9afdac34bdfb7e954896627cf1c04
Marco Del Boccio <marcodelboccio77@gmail.com>
2024-01-30 19:03:29 -0500

    Adding new landing-features-component to landing-page web component

--- web-components/src/layouts/landing/index.js
+++ web-components/src/layouts/landing/index.js
@@ -3,3 +3,4 @@ export * from './src/landing';
 // Sub-components
 export * from './src/landing-header/landing-header';
 export * from './src/landing-hero/landing-hero';
+export * from './src/landing-features/landing-features';
---
+++ web-components/src/layouts/landing/src/landing-features/landing-features.js
@@ -0,0 +1,49 @@
+import {
+  html,
+  mapComponentEvents,
+  updateVars,
+  registerTriggers,
+} from '../../../../global/web-tools';
+
+import componentHtml from './landing-features.html';
+import componentStyle from './landing-features.css';
+
+export default class LandingFeaturesComponent extends HTMLElement {
+  static get observedAttributes() {
+    return ['data-title'];
+  }
+
+  constructor() {
+    super();
+    const template = html`
+      <style>
+        ${componentStyle}
+      </style>
+      ${componentHtml}
+    `;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template);
+  }
+
+  connectedCallback() {
+    mapComponentEvents(this);
+    updateVars(this);
+    registerTriggers(this, (event) => console.log(event));
+  }
+
+  disconnectedCallback() {}
+
+  // Solo los atributos que se retornan en 'observedAttributes' serán observados
+  attributeChangedCallback(name, oldValue, newValue) {
+    // Si el atributo es 'title' y existe un nuevo valor
+    if (name === 'data-title' && newValue) {
+      // Asignar el nuevo valor al html
+      console.log(name, oldValue, newValue);
+      this.shadowRoot.querySelector('#title').textContent = newValue;
+    }
+  }
+
+  adoptedCallback() {}
+}
+
+window.customElements.define('landing-features', LandingFeaturesComponent);
