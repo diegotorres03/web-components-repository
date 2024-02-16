05f40de7a9971bb14f0b716fa3add96aa68652ee
Marco Del Boccio <marcodelboccio77@gmail.com>
2024-02-16 11:47:18 -0500

    adding 'landing-feature-card' component and some svg's

--- web-components/src/layouts/landing/index.js
+++ web-components/src/layouts/landing/index.js
@@ -3,4 +3,8 @@ export * from './src/landing';
 // Sub-components
 export * from './src/landing-header/landing-header';
 export * from './src/landing-hero/landing-hero';
+
 export * from './src/landing-features/landing-features';
+export * from './src/landing-features/landing-feature-card/landing-feature-card';
+
+export * from './src/landing-testimonials/landing-testimonials';
